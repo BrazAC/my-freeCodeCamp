c302f4471c77f70a6624bbbf661d187ab9624a6f
brazAC <braz0502002@gmail.com>
2024-01-14 01:00:57 -0300

    reseting logic progress

--- JavaScript Algorithms and Data Structures/cash register/script.js
+++ JavaScript Algorithms and Data Structures/cash register/script.js
@@ -3,18 +3,3 @@ const changeDiv = document.getElementById('change-due');
 const purchaseButton = document.getElementById('purchase-btn');
 const priceDisplay = document.getElementById('show-price');
 
-let price = 1.87;
-let cash = cashInput.value;
-let cid = [
-    ["PENNY", 1.01],
-    ["NICKEL", 2.05],
-    ["DIME", 3.1],
-    ["QUARTER", 4.25],
-    ["ONE", 90],
-    ["FIVE", 55],
-    ["TEN", 20],
-    ["TWENTY", 60],
-    ["ONE HUNDRED", 100]
-  ];
-
-priceDisplay.textContent = price;
